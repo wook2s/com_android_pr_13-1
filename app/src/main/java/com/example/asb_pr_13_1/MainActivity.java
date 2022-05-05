ace0917b9153704e8d3c0800b763815dcd3f9b04
jadeKim <jade0911k@gmail.com>
2022-05-05 23:01:15 +0900

    added thread and seekbar

--- app/src/main/java/com/example/asb_pr_13_1/MainActivity.java
+++ app/src/main/java/com/example/asb_pr_13_1/MainActivity.java
@@ -7,6 +7,7 @@ import android.Manifest;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,18 +15,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listViewMP3;
     Button btnPlay, btnStop;
-    TextView tvMP3;
-    ProgressBar pbMP3;
+    TextView tvMP3, tvTime;
+    SeekBar pbMP3;
 
     ArrayList<String> mp3List;
     String selectedMP3;
@@ -70,12 +73,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //mp3List.add("123");
-        selectedMP3 = mp3List.get(0);
+        if(mp3List.size() != 0){
+            selectedMP3 = mp3List.get(0);
+        }
 
         btnPlay = (Button) findViewById(R.id.btnplay);
         btnStop = (Button) findViewById(R.id.btnStop);
         tvMP3 = (TextView) findViewById(R.id.tvMP3);
-        pbMP3 = (ProgressBar) findViewById(R.id.pbMP3);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+
+        pbMP3 = (SeekBar) findViewById(R.id.pbMP3);
 
         btnStop.setClickable(false);
 
@@ -95,6 +102,9 @@ public class MainActivity extends AppCompatActivity {
 
                     tvMP3.setText("실행중인 음악 : " + selectedMP3);
                     pbMP3.setVisibility(View.VISIBLE);
+
+                    T1 t1 = new T1(pbMP3, mediaPlayer, tvTime);
+                    t1.start();
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -117,5 +127,81 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        pbMP3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(b == true){
+                    mediaPlayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+    class T1 extends Thread{
+        ProgressBar pb;
+        MediaPlayer mediaPlayer;
+        TextView tvTime;
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
+
+        T1(ProgressBar pb, MediaPlayer mediaPlayer, TextView tvTime){
+            this.pb = pb;
+            this.mediaPlayer = mediaPlayer;
+            this.tvTime = tvTime;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            if(mediaPlayer == null){
+                return;
+            }else{
+                pb.setMax(mediaPlayer.getDuration());
+                while(mediaPlayer.isPlaying()){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            pb.setProgress(mediaPlayer.getCurrentPosition());
+                            tvTime.setText("진행시간 : " + timeFormat.format(mediaPlayer.getCurrentPosition()));
+                        }
+                    });
+                    SystemClock.sleep(200);
+                }
+
+            }
+
+
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
